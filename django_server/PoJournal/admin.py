478805489a640d1911c6f463bad3e30609d3fa5e
KirkNotCaptain <cmariekirk@gmail.com>
2021-05-09 12:16:20 -0400

    Created admin model for Journal

--- django_server/PoJournal/admin.py
+++ django_server/PoJournal/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Journal
+
+
+class JournalAdmin(admin.ModelAdmin):
+    list_display = ('title', 'body', 'date')
+
 
 # Register your models here.
+admin.site.register(Journal, JournalAdmin)
